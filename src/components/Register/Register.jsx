dbebae5d7452838b66d5ea1fc239c6728ebfa47c
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-11-20 05:21:31 +0600

    add toggle show pass and accept terms and conditions

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import { auth } from "../../firebase/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { LuEye, LuEyeClosed } from "react-icons/lu";
 
 const Register = () => {
   // form input states
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [acceptTerms, setAcceptTerms] = useState(false);
 
   // UI state
   const [loading, setLoading] = useState(false);
   const [signupSuccess, setSignupSuccess] = useState(false);
   const [signupError, setSignupError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [termsError, setTermsError] = useState("");
 
   const handleRegister = async (event) => {
     event.preventDefault();
@@ -23,6 +27,7 @@ const Register = () => {
     setSignupSuccess(false);
     setEmailError("");
     setPasswordError("");
+    setTermsError("");
 
     const emailValue = email.trim();
 
@@ -35,18 +40,24 @@ const Register = () => {
     if (emailValue && !emailPattern.test(emailValue))
       emailErrors.push("Invalid email address");
 
+    if (!password) passwordErrors.push("Password is required");
+    if (password && password.length < 8)
+      passwordErrors.push(
+        "Password must be at least 8 characters and include at least one uppercase, one lowercase, and one special character."
+      );
+    const passwordPattern =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+    if (password && !passwordPattern.test(password))
+      passwordErrors.push("Password is too weak");
+
+    if (!acceptTerms) {
+      setTermsError("You must accept the Terms and Conditions");
+    }
+
     if (emailErrors.length > 0) {
       setEmailError(emailErrors.join(`. `));
       return;
     }
-
-    if (!password) passwordErrors.push("Password is required");
-    if (password && password.length < 8)
-      passwordErrors.push("Password must be at least 8 characters");
-    const passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
-    if (password && !passwordPattern.test(password))
-      passwordErrors.push("Password is too weak");
-
     if (passwordErrors.length > 0) {
       setPasswordError(passwordErrors.join(`. `));
       return;
@@ -59,6 +70,7 @@ const Register = () => {
       setSignupSuccess(true);
       setEmail("");
       setPassword("");
+      setAcceptTerms(false);
       setSignupError("");
     } catch (error) {
       // friendly mapping
@@ -72,6 +84,12 @@ const Register = () => {
       setLoading(false);
     }
   };
+
+  const handleTogglePasswordShow = (event) => {
+    event.preventDefault();
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -87,9 +105,10 @@ const Register = () => {
                 name="email"
                 className="input"
                 placeholder="Email"
+                value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 aria-invalid={!!emailError}
-                aria-describedby="email-error"
+                aria-describedby={emailError ? "email-error" : undefined}
               />
 
               {emailError && (
@@ -97,21 +116,64 @@ const Register = () => {
               )}
 
               <label className="label">Password</label>
-              <input
-                type="password"
-                name="password"
-                className="input"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                aria-invalid={!!passwordError}
-                aria-describedby="password-error"
-              />
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  className="input"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  aria-invalid={!!passwordError}
+                  aria-describedby={
+                    passwordError ? "password-error" : undefined
+                  }
+                />
+                <button
+                  type="button"
+                  onClick={handleTogglePasswordShow}
+                  className="btn btn-xs bg-transparent border-none absolute top-2 right-6"
+                  aria-label={showPassword ? "Hide password" : "Show password"}
+                >
+                  {showPassword ? <LuEyeClosed /> : <LuEye />}
+                </button>
+              </div>
 
               {passwordError && (
                 <p className="text-red-500 font-bold text-xl">
                   {passwordError}
                 </p>
               )}
+
+              <div>
+                <fieldset className="fieldset">
+                  <label className="label flex items-center gap-2">
+                    <input
+                      id="acceptTerms"
+                      type="checkbox"
+                      className="checkbox"
+                      checked={acceptTerms}
+                      onChange={(e) => setAcceptTerms(e.target.checked)}
+                      aria-invalid={!!termsError}
+                      aria-describedby={termsError ? "terms-error" : undefined}
+                    />
+                    <span>Accept our Terms and Conditions</span>
+                  </label>
+                  {termsError && (
+                    <p
+                      id="terms-error"
+                      className="text-red-500 font-medium text-sm"
+                    >
+                      {" "}
+                      {termsError}{" "}
+                    </p>
+                  )}
+                </fieldset>
+              </div>
+
+              <div className="flex justify-center items-center">
+                <a className="link link-hover"> Forgot password? </a>
+              </div>
 
               <fieldset disabled={loading}>
                 <button className="btn btn-neutral mt-4">
